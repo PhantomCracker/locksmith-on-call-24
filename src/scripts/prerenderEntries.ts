88b5df1f8c3ad32cc5f0fc02c51fae9c23b0278a
Phantom Cracker <popa.vlad94@gmail.com>
2025-06-22 11:47:26 +0300

    add configs to sveltekit

---
+++ src/scripts/prerenderEntries.ts
@@ -0,0 +1,11 @@
+// prerenderEntries.js
+export const entries = [
+    '/',
+    '/about',
+    '/areas-covered',
+    '/services',
+    '/locksmith-in-blackpool',
+    '/locksmith-in-gloucester',
+    '/locksmith-in-preston',
+    '/locksmith-in-swindon'
+];
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,23 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+import { entries } from './src/scripts/prerenderEntries.ts';
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			entries
+		}),
+		paths: {
+			base: ''
+		}
+	}
 };
 
 export default config;
