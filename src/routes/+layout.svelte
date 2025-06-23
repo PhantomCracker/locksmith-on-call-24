13c41cf313bfc1eb6b3a632ecc5db0b546d9af76
Phantom Cracker <popa.vlad94@gmail.com>
2025-06-23 11:11:15 +0300

    add new icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,8 @@
 <svelte:head>
 	<title>Emergency Locksmith Near You</title>
 	<meta name="description" content="Locksmith on Call offers 24/7 emergency locksmith services with no call-out fees. Fast, reliable & local." />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
 	<link rel="preload" as="image" href="/logo.png" />
 	<link
 		rel="preload"
@@ -172,14 +174,14 @@
 				<a href="/about" class="hover:text-primary transition">About</a>
 				<a href="/areas-covered" class="hover:text-primary transition">Covered areas</a>
 				<a href="tel:0123456789" class="bg-accent text-white px-4 py-2 rounded hover:opacity-90 transition flex items-center gap-1">
-					<span class="material-symbols-outlined">call</span>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2c2c2c"><path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/></svg>
 					Call Now
 				</a>
 			</nav>
 
 			<!-- Mobile Menu Button -->
-			<button class="md:hidden" on:click={() => mobileNavOpen = !mobileNavOpen}>
-				<span class="material-symbols-outlined text-3xl text-primary">menu</span>
+			<button class="md:hidden" on:click={() => mobileNavOpen = !mobileNavOpen} aria-label="Menu">
+				<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-primary" height="24px" viewBox="0 -960 960 960" width="24px" fill="#004aad"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
 			</button>
 		</div>
 
