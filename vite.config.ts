7c169453584127cf335cf7b838d1d55d33d5fa3d
Phantom Cracker <popa.vlad94@gmail.com>
2025-06-23 10:19:48 +0300

    lazy loading of css and js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { beasties } from 'vite-plugin-beasties'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		beasties({
+			options: {
+				preload: 'swap',
+			}
+		})
+	]
 });
