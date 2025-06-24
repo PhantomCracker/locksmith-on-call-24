dc06f69b91202fb7d2c9bdaa5305c6c29847f277
Phantom Cracker <popa.vlad94@gmail.com>
2025-06-24 08:34:53 +0300

    add gtm tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,16 @@
 	<meta name="description" content="Locksmith on Call offers 24/7 emergency locksmith services with no call-out fees. Fast, reliable & local." />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KRWFHND7');
+	</script>
+	<!-- End Google Tag Manager -->
+
 	<link rel="preload" as="image" href="/logo.webp" />
 	<link
 		rel="preload"
